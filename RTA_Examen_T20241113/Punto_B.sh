74a0fa8c4d9694ee9016c9e3d1a75036b488b334
noecoro <coronoelia.@gmail.com>
2024-11-17 12:51:54 -0500

    Actualización script Punto_B.sh

--- RTA_Examen_T20241113/Punto_B.sh
+++ RTA_Examen_T20241113/Punto_B.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 # Comandos para el punto B
+
+# Crear el script AltaUser-Groups.sh en la ruta especificada
+sudo vim /usr/local/bin/Coro/AltaUser-Groups.sh
+
+# Dar permisos de ejecución al script
+sudo chmod +x /usr/local/bin/Coro/AltaUser-Groups.sh
+
+# Ejecutar el script para crear usuarios y grupos
+sudo /usr/local/bin/Coro/AltaUser-Groups.sh osboxes /home/osboxes/UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt
+
+# Comprobaciones
+cat /home/osboxes/UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt
+
+# Guardar el historial de comandos en un archivo
+history > comandosScript.txt
+
