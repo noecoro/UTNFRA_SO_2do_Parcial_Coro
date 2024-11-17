39ad24516a9fa60ee40c734aa0ee3f247f55c6cc
noecoro <coronoelia.@gmail.com>
2024-11-17 12:54:40 -0500

    Actualización Punto_C.sh

--- RTA_Examen_T20241113/Punto_C.sh
+++ RTA_Examen_T20241113/Punto_C.sh
@@ -1,1 +1,15 @@
 # Comandos para el punto C
+#!/bin/bash
+
+# Verificar la version de Docker instalada
+docker --version
+
+# Cambiar al directorio de configuracion de Docker
+cd /home/osboxes/UTN-FRA_SO_Examenes/202406/docker
+
+# Editar el archivo index.html
+sudo vim index.html
+
+# Mostrar el contenido del archivo index.html
+cat index.html
+
